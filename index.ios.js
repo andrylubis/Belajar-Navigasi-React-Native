4e58c6a03de724b1236292974e9a45f5c4324839
Sandi Andrian <andrian.sandi@gmail.com>
2017-06-01 01:48:01 +0700

    styling button fixed footer

--- index.ios.js
+++ index.ios.js
@@ -9,24 +9,34 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  Alert,
+  Button
 } from 'react-native';
 
 export default class BelajarNavigasiReact extends Component {
+
+  onClickNext() {
+    Alert.alert('Mari menuju ke splash screen');
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Halo, mari belajar React Native
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={ styles.buttonWrapper }>
+          <TouchableHighlight 
+            style={styles.button}
+            underlayColor="#ff6d00"
+            onPress={ this.onClickNext }>
+            <Text style={styles.buttonText}>Next</Text>
+          </TouchableHighlight>
+        </View>
       </View>
+
     );
   }
 }
@@ -36,18 +46,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fff'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  buttonWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 0,
+    left: 0, 
+    right: 0
   },
+  button: {
+    flexDirection: 'row',
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff6d00',
+  },
+  buttonText: {
+    color: 'white'
+  }
 });
 
 AppRegistry.registerComponent('BelajarNavigasiReact', () => BelajarNavigasiReact);
